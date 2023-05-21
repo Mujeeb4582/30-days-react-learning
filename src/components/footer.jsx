b88689108ced27c357b73f26ba690c4b05eec03f
Mujeeb4582 <mujeeburahman4582@gmail.com>
2023-05-22 00:07:38 +0500

    Add the footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+const copyRight = "2023";
+
+const Footer = () => (
+  <footer>
+    <div className="footer-wrapper">
+      <p>Copyright &copy;{copyRight}</p>
+    </div>
+  </footer>
+);
+
+export default Footer;
