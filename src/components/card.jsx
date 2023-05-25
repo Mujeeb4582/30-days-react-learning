fe0ef0d9759fcc29f1a4ffe027868234031928e1
Mujeeb4582 <mujeeburahman4582@gmail.com>
2023-05-25 08:53:32 +0500

    Add the solution for Level-3 queston-2

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,13 +1,77 @@
 import mujeebimage from "../images/Mujeeb Profile Picture.jpg";
 import { student } from "./header";
 
-const UserCard = () => (
-  <div className="user-card">
-    <img src={mujeebimage} alt="mujeeb" />
-    <h2>
-      {student.firstName} {student.lastName}
-    </h2>
-  </div>
-);
+const UserCard = () => {
+  const skillsArray = [
+    "HTML",
+    "CSS",
+    "SCSS",
+    "JS",
+    "React",
+    "Redux",
+    "Node",
+    "MongDB",
+    "Python",
+    "Flask",
+    "Djongo",
+    "NumPy",
+    "Pandas",
+    "Data Anaylsis",
+    "MySQL",
+    "GraphQL",
+    "D3.js",
+    "Gatsby",
+    "Docker",
+    "Heroku",
+    "Git",
+  ];
 
+  const skillsFormated = skillsArray.map((skill) => (
+    <li
+      key={skill}
+      style={{
+        padding: "0.3rem 0.4rem",
+        backgroundColor: "#6cf",
+        color: "#fff",
+        wordWrap: "normal",
+        borderRadius: "10px",
+      }}
+    >
+      {skill}
+    </li>
+  ));
+  return (
+    <div className="user-card">
+      <img src={mujeebimage} alt="mujeeb" />
+      <h2>
+        {student.firstName} {student.lastName}{" "}
+        <span
+          style={{
+            backgroundColor: "#6cf",
+            color: "#fff",
+            borderRadius: "50%",
+          }}
+        >
+          &#x2713;
+        </span>
+      </h2>
+      <p style={{ lineHeight: "80px" }}>
+        Student Full-Stack-Developer, Pakistan
+      </p>
+      <h2 style={{ marginBottom: "30px" }}>Skills</h2>
+      <ul
+        style={{
+          display: "flex",
+          gap: "10px",
+          alignItems: "center",
+          flexWrap: "wrap",
+          marginBottom: "30px",
+        }}
+      >
+        {skillsFormated}
+      </ul>
+      <div>&#128337; Joined on May 22, 2023</div>
+    </div>
+  );
+};
 export default UserCard;
